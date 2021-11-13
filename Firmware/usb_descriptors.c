80bd981e9d9385c6a3d9860d8d2596c5be6f7e1e
Darth Affe <darthaffe@wyrez.org>
2021-11-14 00:47:35 +0100

    Updated to compile against the current pico-sdk

--- Firmware/usb_descriptors.c
+++ Firmware/usb_descriptors.c
@@ -97,7 +97,7 @@ uint8_t const desc_configuration[] =
     {
         // Config number, interface count, string index, total length, attribute, power in mA
         TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 250),
-        TUD_HID_INOUT_DESCRIPTOR(ITF_NUM_HID, 0, HID_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID, 0x80 | EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 1),
+        TUD_HID_INOUT_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID, 0x80 | EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 1),
 #if CFG_TUD_VENDOR > 0
         TUD_VENDOR_DESCRIPTOR(ITF_NUM_DATA, 1, USBD_DATA_EP_OUT, USBD_DATA_EP_IN, 64),
 #endif
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Firmware for the Raspberry Pi Pico (RP2040) to be used with [RGB.NET](https://github.com/DarthAffe/RGB.NET).
 
 
-Building the firmware requires the [pico sdk](https://github.com/raspberrypi/pico-sdk) and a recent version of [tinyusb](https://github.com/hathach/tinyusb).
+Building the firmware requires the [pico sdk](https://github.com/raspberrypi/pico-sdk).
 
 For precompiled binaries check https://github.com/DarthAffe/RGB.NET-PicoPi/releases
